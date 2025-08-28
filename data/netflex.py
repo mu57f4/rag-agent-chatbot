676189b84f2626ff9de3535a23bdaaebb7037d88
Mustafa Ahmed <mustafataha874@gmail.com>
2025-08-28 15:49:22 +0300

    init spider class

---
+++ data/netflex.py
@@ -0,0 +1,39 @@
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
+
+class NetflexSpider(CrawlSpider):
+
+    name = "netflex"
+    base_url = "https://help.netflix.com/en"
+    start_urls = ["https://help.netflix.com/en"]
+    allowd_domains = ["help.netflix.com"]
+    max_pages = 100
+    counter = 0
+
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True)
+    )
+
+    # to parse html content
+    def parse_item(self, reponse):
+        if self.counter > self.max_pages:
+            raise CloseSpider("Page limit reached")
+        
+        data = reponse.css('.left-pane').getall()
+
+        page_title = reponse.css('title::text').get()
+
+        # to save file with page title
+        if (not page_title) or (not data) or (not isinstance(data, list)) or (len(data) == 0):
+            return
+
+        # preprocessing title        
+        page_title = page_title.replace(' ', '_').replace('/', '_').replace('//', '_').strip()
+
+        print(f"Downloading {page_title}:\n{reponse.url}")
+
+        file_name = 'netflex_data/' + page_title + '.html'
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('\n'.join(data))

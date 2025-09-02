f405fe1beba5d5b8874af76f01a7b093671dd49b
Mustafa Ahmed <mustafataha874@gmail.com>
2025-09-02 16:41:39 +0300

    Init streamlit app code

---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+from src.crew import chat_crew
+import time
+import src.crew.utils as utils
+
+st.title("Netflex Customer Support Chatbot")
+
+# Sidebar
+st.sidebar.header("Customer Info")
+
+if "customer" not in st.session_state:
+    st.session_state.customer = {}
+
+with st.sidebar.form("customer_form", clear_on_submit=False):
+    first_last_name = st.text_input("First Last Name", placeholder="Khalid Ali")
+    username = st.text_input("Username", placeholder="unique pre customer, e.g. khalid.ali")
+    subscription_plan = st.selectbox("Subscription Plan", ["No Plan", "Basic", "Standard", "Premium"])
+
+    submitted = st.form_submit_button("Save Customer")
+    if submitted:
+        st.session_state.customer = {
+            "name": first_last_name,
+            "username": username,
+            "subscription_plan": subscription_plan,
+        }
+        utils.add_or_update_subscription(
+            customer_id=username,
+            subscription_plan=subscription_plan
+        )
+        st.sidebar.success("Data saved!")
+
+if st.sidebar.button("🔄 Refresh"):
+    st.rerun()
+
+if st.session_state.customer:
+    st.sidebar.warning(f"Your current subscription plan is: {utils.get_subscription(st.session_state.customer.get('username'))}")
+
+# Main Chat
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Show past messages
+for role, msg in st.session_state.chat_history:
+    with st.chat_message(role):
+        st.markdown(msg)
+
+customer_ready = bool(st.session_state.customer.get("username")) and bool(st.session_state.customer.get("name"))
+
+def convert_chat_history(history_tuples):
+    return [{"role": role, "content": msg} for role, msg in history_tuples]
+
+if not customer_ready:
+    st.error("Please save your customer info in the sidebar before chatting.")
+    st.chat_input("Type your message...", disabled=True)
+
+else:
+
+    # Chat input
+    if user_input := st.chat_input("Type your message..."):
+        # Store user message
+        st.session_state.chat_history.append(("user", user_input))
+        with st.chat_message("user"):
+            st.markdown(user_input)
+        
+        # print(type(st.session_state.chat_history))
+
+        chat_history = convert_chat_history(st.session_state.chat_history)
+        current_subscription_plan = utils.get_subscription(st.session_state.customer.get('username'))
+
+        try:
+            support_reply = chat_crew(
+                customer_name=st.session_state.customer.get('name'),
+                customer_id=st.session_state.customer.get('username'),
+                customer_question=user_input,
+            )
+
+        except Exception as e:
+            support_reply = "Sorry, there was an error in the backend. Please try again."
+            st.error(f"Error: {e}")
+            
+        def stream_reply():
+            for chunk in support_reply.split(" "):
+                yield chunk + " "
+                time.sleep(0.05)
+
+        st.session_state.chat_history.append(("assistant", support_reply))
+        with st.chat_message("assistant"):
+            st.write_stream(stream_reply)

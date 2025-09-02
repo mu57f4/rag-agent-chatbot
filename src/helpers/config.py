66dd2f2c21442b4feba930643f46cc2b18e1dd97
Mustafa Ahmed <mustafataha874@gmail.com>
2025-09-02 06:34:54 +0300

    To retrive env vars

---
+++ src/helpers/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    CHROMA_API_KEY: str
+    CHROMA_TENANT: str
+    CHROMA_DATABASE: str
+    GROQ_API_KEY: str
+    AGENTOPS_API_KEY: str
+    OPENAI_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()

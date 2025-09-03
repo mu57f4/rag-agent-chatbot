b55b023b1d266b79ac1920547a2fecaceb4759a2
Mustafa Ahmed <mustafataha874@gmail.com>
2025-09-03 22:27:45 +0300

    Configuration to load the env vars

---
+++ src/api/helpers/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    CHROMA_API_KEY: str
+    CHROMA_TENANT: str
+    CHROMA_DATABASE: str
+    GEMINI_API_KEY: str
+    APP_NAME: str
+    APP_VERSION: str
+
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()
